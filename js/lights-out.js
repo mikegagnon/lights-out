b00ae389741e04224df280d1d2070bed62b0fbe2
Michael Gagnon <mikegagnon@gmail.com>
2016-09-24 22:33:11 -0600

    Random lights

--- js/lights-out.js
+++ js/lights-out.js
@@ -1,4 +1,30 @@
 
+var numRows = 4
+var numCols = 5
+
+var matrix = [
+    [true, true, true, true],
+    [true, true, true, true],
+    [true, true, true, true],
+    [true, true, true, true]
+]
+
+for (var row = 0; row < numRows; row++) {
+    for (var col = 0; col < numCols; col++) {
+
+        var lightStatus = Math.random() < 0.5
+        matrix[row][col] = lightStatus
+
+        var lightId = "light-" + row + "-" + col
+        if (lightStatus) {
+            $("#" + lightId).css("background-color", "pink")
+        } else {
+            $("#" + lightId).css("background-color", "gray")
+        }
+
+    }
+}
+
 function lightClick(row, col) {
     console.log(row, col)
 }

3cd294746f7ec6fca2e27ba60bca32497184b8c6
Michael Gagnon <mikegagnon@gmail.com>
2016-12-10 13:02:05 -0700

    setLightColor function

--- js/lights-out.js
+++ js/lights-out.js
@@ -22,20 +22,23 @@ function getLightId(row, col) {
     return "#light-" + row + "-" + col
 }
 
+function setLightColor(row, col) {
+
+    var lightId = getLightId(row, col)
+
+    if (matrix[row][col] ) {
+        $(lightId).css("background-color", "pink")
+    } else {
+        $(lightId).css("background-color", "gray")
+    }
+}
+
 for (var row = 0; row < numRows; row++) {
     for (var col = 0; col < numCols; col++) {
 
-        var lightStatus = Math.random() < 0.5
-        matrix[row][col] = lightStatus
-
-        var lightId = getLightId(row, col)
-
-        if (lightStatus) {
-            $(lightId).css("background-color", "pink")
-        } else {
-            $(lightId).css("background-color", "gray")
-        }
-
+        matrix[row][col] = Math.random() < 0.5
+        
+        setLightColor(row, col)
     }
 }
 
@@ -76,18 +79,8 @@ function right(col) {
 }
 
 function lightSwitch(row, col) {
-
     matrix[row][col] = !matrix[row][col]
-
-    var lightStatus = matrix[row][col]
-    var lightId = getLightId(row, col)
-
-    if (lightStatus) {
-        $(lightId).css("background-color", "pink")
-    } else {
-        $(lightId).css("background-color", "gray")
-    }
-
+    setLightColor(row, col)
 }
 
 function checkWin() {
